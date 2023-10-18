b108237df2ca1ec7dc0e61d97fd2c98dbe490906
kimdev <kimdev1242@gmail.com>
2023-10-18 17:19:00 +0900

    first commit

---
+++ src/test/java/com/practice/psaa_batch/PsaaBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.psaa_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsaaBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
