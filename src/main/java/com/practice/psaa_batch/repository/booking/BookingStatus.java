e9e532a5ffc6551d2b3ba8232d935bfb7da04d57
kimdev <kimdev1242@gmail.com>
2023-10-19 20:50:04 +0900

    배치 이용시간에 따른 완료 처리
    item reader/writer 및 chunk oriented step  구현

--- src/main/java/com/practice/psaa_batch/repository/booking/BookingStatus.java
+++ src/main/java/com/practice/psaa_batch/repository/booking/BookingStatus.java
@@ -1,5 +1,5 @@
 package com.practice.psaa_batch.repository.booking;
 
 public enum BookingStatus {
-    READY, IN_PROGRESS, COMPLETED, CANCELLED
+    READY, PROGRESSED, COMPLETED, CANCELLED
 }
--- src/main/java/com/practice/psaa_batch/repository/pass/PassStatus.java
+++ src/main/java/com/practice/psaa_batch/repository/pass/PassStatus.java
@@ -1,5 +1,5 @@
 package com.practice.psaa_batch.repository.pass;
 
 public enum PassStatus {
-    READY, IN_PROGRESS, EXPIRED
+    READY, PROGRESSED, EXPIRED
 }
